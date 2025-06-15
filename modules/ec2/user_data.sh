8b3f9ef421214c9f7830bcb9d0ee213cf09ac184
Raj Patel <rajpatel@example.com>
2025-06-15 17:35:20 -0400

    Initial commit: Multi-env Terraform app with Stylish WebApp

--- modules/ec2/user_data.sh
+++ modules/ec2/user_data.sh
@@ -1,17 +1,19 @@
-### ec2/user_data.sh
 #!/bin/bash
 yum update -y
-yum install -y docker unzip
+yum install -y docker git
 systemctl start docker
 systemctl enable docker
 cd /home/ec2-user
-mkdir app
-cd app
-curl -O https://your-bucket.s3.amazonaws.com/stylish-1.0.0.zip
-unzip stylish-1.0.0.zip
+
+# Clone your GitHub repo
+git clone https://github.com/patelraj-41299/cap-4.git
+cd cap-4/app/frontend/stylish-1.0.0
+
+# Create Dockerfile
 cat <<EOF > Dockerfile
 FROM nginx:alpine
 COPY . /usr/share/nginx/html
 EOF
+
 docker build -t stylish-app .
 docker run -d -p 80:80 stylish-app
