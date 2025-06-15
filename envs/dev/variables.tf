d016420a87e3db8845ba48dbce5570e38112cbb9
Raj Patel <rajpatel@example.com>
2025-06-15 19:37:29 -0400

    Clean commit: use GitHub Secrets for AWS credentials

---
+++ envs/dev/variables.tf
@@ -0,0 +1,34 @@
+variable "env" {
+  description = "Deployment environment (dev, staging, prod)"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
