85822446d7a33e6881029ff213025f1d6dae6ec7
Raj Patel <rajpatel@example.com>
2025-06-16 21:42:13 -0400

    Fix codebuild module with correct variables

--- modules/codebuild/main.tf
+++ modules/codebuild/main.tf
@@ -1,19 +1,17 @@
 resource "aws_codebuild_project" "this" {
   name          = "${var.env}-codebuild"
-  description   = "Terraform CodeBuild project for ${var.env} environment"
-  build_timeout = 30
-
   service_role  = var.codebuild_role_arn
 
   artifacts {
-    type = "NO_ARTIFACTS"
+    type = "S3"
+    location = var.artifact_bucket
+    packaging = "ZIP"
   }
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:6.0"
+    image                       = "aws/codebuild/standard:5.0"
     type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
   }
 
@@ -21,10 +19,6 @@ resource "aws_codebuild_project" "this" {
     type            = "GITHUB"
     location        = var.repo_url
     git_clone_depth = 1
-    buildspec       = "envs/${var.env}/buildspec.yml"
-  }
-
-  tags = {
-    Environment = var.env
+    buildspec       = var.buildspec_location
   }
 }
--- modules/codebuild/variables.tf
+++ modules/codebuild/variables.tf
@@ -1,11 +1,29 @@
 variable "env" {
-  type = string
-}
-
-variable "repo_url" {
-  type = string
+  description = "Environment name (e.g., dev, staging, prod)"
+  type        = string
 }
 
 variable "codebuild_role_arn" {
-  type = string
+  description = "IAM role ARN for CodeBuild"
+  type        = string
+}
+
+variable "artifact_bucket" {
+  description = "S3 bucket name to store CodeBuild artifacts"
+  type        = string
+}
+
+variable "repo_url" {
+  description = "GitHub repository URL"
+  type        = string
+}
+
+variable "branch" {
+  description = "GitHub branch to build from"
+  type        = string
+}
+
+variable "buildspec_location" {
+  description = "Path to buildspec.yml file"
+  type        = string
 }
