8b17595de785db1b1b58ce22a9b6f087f32cc118
Raj Patel <rajpatel@example.com>
2025-06-16 21:37:09 -0400

    Add complete CodePipeline Terraform module

--- modules/codepipeline/variables.tf
+++ modules/codepipeline/variables.tf
@@ -1,8 +1,34 @@
-variable "name" {}
-variable "role_arn" {}
-variable "artifact_bucket" {}
-variable "connection_arn" {}
-variable "repository" {}
-variable "branch" {}
-variable "codebuild_project" {}
+variable "name" {
+  description = "Name of the CodePipeline"
+  type        = string
+}
 
+variable "role_arn" {
+  description = "IAM role ARN for CodePipeline"
+  type        = string
+}
+
+variable "artifact_bucket" {
+  description = "S3 bucket name for storing artifacts"
+  type        = string
+}
+
+variable "connection_arn" {
+  description = "CodeStar connection ARN"
+  type        = string
+}
+
+variable "repository" {
+  description = "GitHub repository in format owner/repo"
+  type        = string
+}
+
+variable "branch" {
+  description = "Branch name to trigger pipeline"
+  type        = string
+}
+
+variable "codebuild_project" {
+  description = "CodeBuild project name"
+  type        = string
+}
