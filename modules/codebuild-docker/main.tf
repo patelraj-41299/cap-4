bc9d50aae7bbb8917766fe1a2021eea669413f17
Raj Patel <rajpatel@example.com>
2025-06-18 16:38:09 -0400

    Add main.tf and outputs.tf for Docker CodeBuild module

---
+++ modules/codebuild-docker/main.tf
@@ -0,0 +1,45 @@
+resource "aws_codebuild_project" "this" {
+  name         = "cap4-${var.env}-docker"
+  description  = "Build and push Docker image to ECR for ${var.env}"
+  service_role = var.codebuild_role_arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:7.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true
+    environment_variables = [
+      {
+        name  = "REPO_URL"
+        value = var.repo_url
+      },
+      {
+        name  = "BRANCH"
+        value = var.branch
+      },
+      {
+        name  = "ENV"
+        value = var.env
+      }
+    ]
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = var.repo_url
+    git_clone_depth = 1
+    buildspec       = var.buildspec_location
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = "/aws/codebuild/cap4-${var.env}-docker"
+      stream_name = "build-log"
+    }
+  }
+}
+
---
+++ modules/codebuild-docker/outputs.tf
@@ -0,0 +1,4 @@
+output "project_name" {
+  value = aws_codebuild_project.this.name
+}
+
